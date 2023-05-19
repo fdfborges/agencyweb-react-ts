9d1026080892e7ea6071580906e29f3a0b7a6dbc
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-19 13:43:18 -0300

    Add and Finaly Header

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,24 @@
+import styles from './header.module.css'
+import LogoIpsum from '../../../public/assets/logo.svg'
+
+export function Header() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src={LogoIpsum} alt="" />
+                
+
+            </div>
+            <div className={styles.buttonsContainer}>
+                <button className={styles.buttons}>Home</button>
+                <button className={styles.buttons}>Sobre nós</button>
+                <button className={styles.buttons}>Serviços</button>
+                <button className={styles.buttons}>Blog</button>
+                <button className={styles.buttons}>Contate-nos</button>
+                
+            </div>
+        </div>
+
+    )
+
+}
