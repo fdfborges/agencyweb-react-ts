dcb44b21c1ef29abe4b37d60d5340821a787e96e
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-19 14:20:27 -0300

    Updating

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,5 +3,6 @@ import { Header } from "../../components/Header/header";
 export function Home(){
     return(
         <Header />
+        
     )
 }
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -15,7 +15,7 @@ export function Header() {
                 <button className={styles.buttons}>Serviços</button>
                 <button className={styles.buttons}>Blog</button>
                 <button className={styles.buttons}>Contate-nos</button>
-                
+                <h1>Helo</h1>
             </div>
         </div>
 
