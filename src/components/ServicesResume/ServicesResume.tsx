8c3c07aa395f59c206c9439955c750f2b2063106
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-22 15:25:27 -0300

    Create and add MobileCardResume

--- src/components/ServicesResume/ServicesResume.tsx
+++ src/components/ServicesResume/ServicesResume.tsx
@@ -1,9 +1,11 @@
 import { ApresentationResume } from "./ApresentationResume/ApresentationResume";
+import { MobileResume } from "./MobileResume/MobileResume";
 
 export function ServicesResume(){
     return(
         <>
         <ApresentationResume />
+        <MobileResume />
         </>
     )
 }
