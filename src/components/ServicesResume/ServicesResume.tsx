003aacd2ea8deba6d2ab5066cf596efea325a6b8
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-22 16:17:53 -0300

    Resolving Problems from component webMobile

--- src/components/ServicesResume/ServicesResume.tsx
+++ src/components/ServicesResume/ServicesResume.tsx
@@ -1,11 +1,13 @@
 import { ApresentationResume } from "./ApresentationResume/ApresentationResume";
 import { MobileResume } from "./MobileResume/MobileResume";
+import { WebMobileResume } from "./WebMobileResume/WebMobileResume";
 
 export function ServicesResume(){
     return(
         <>
         <ApresentationResume />
         <MobileResume />
+        <WebMobileResume />
         </>
     )
 }
