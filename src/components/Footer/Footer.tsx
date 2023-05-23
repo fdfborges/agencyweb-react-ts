f248e4eb635223379cf8b35e8cc1be333dfb1839
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-23 13:29:15 -0300

    Create and Add component footer to page Home

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,7 +9,39 @@ import twitter from '../../../public/assets/FooterImgs/twitter.svg';
 export function Footer() {
     return (
         <>
-           
+            <footer>
+                <section className={styles.content}>
+                    <div className={styles.left}>
+                        <a href="#"><img src={Logo} alt="" /></a>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                        <p>@Lorem</p>
+                    </div>
+                    <div className={styles.half}>
+                        <ul>
+                            <li><h3>Sobre Nós</h3></li>
+                            <li><a href="#">Lorem</a></li>
+                            <li><a href="#">Portifólio</a></li>
+                            <li><a href="#">Carrers</a></li>
+                            <li><a href="#">Cont</a></li>
+                        </ul>
+                    </div>
+                    <div className={styles.right}>
+                        <h3>Contate-nos</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                        <p><a href="tel:+90889097890">@+908 89097 890</a></p>
+                    </div>
+                    <div className={styles.social}>
+
+                        <a href="#"><button><img src={Facebook} alt="" /></button></a>
+                        <a href="#"><button><img src={Instagram} alt="" /></button></a>
+                        <a href="#"><button><img src={twitter} alt="" /></button></a>
+                        <a href="#"><button><img src={Linkedin} alt="" /></button></a>
+                    </div>
+                </section>
+                <div className={styles.baseboard}>
+                    <p>Feito com ❤️ por Felipe Borges</p>
+                </div>
+            </footer>
         </>
     )
 }
