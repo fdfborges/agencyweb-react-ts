3f5c2fb4abfe46dec053f41421e79905098b5c29
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-24 11:04:49 -0300

    Preparing fot deploy

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import styles from './Newsletter.module.css';
 
 export function Newsletter() {
+
+
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+
+
+        setInputValue('');
+    }
+
     return (
         <>
             <div className={styles.container}>
                 <h2>Lorem Ipsum is simply dummy <br /> text of the printing. </h2>
-                <form className={styles.inputandbtn}>
-                    <input type="email" placeholder='Seu melhor e-mail' />
-                    <button type='submit'>INSCREVA-SE</button>
+                
+                <form onSubmit={handleSubmit}
+                className={styles.inputandbtn}>
+
+                    <input 
+                    type="email"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder='Seu melhor e-mail' />
+                    
+                    <button
+                    type='submit'>
+                    INSCREVA-SE
+                    </button>
                 </form>
 
 
