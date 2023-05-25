77802ff7a09fcdaff2078f306e024f79e863cb37
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-25 16:54:38 -0300

    Create and add component ParagraphBlock

--- src/Pages/AboutUs/AboutUs.tsx
+++ src/Pages/AboutUs/AboutUs.tsx
@@ -1,10 +1,12 @@
 import { AboutUsApresentation } from "../../components/AboutUsApresentation/AboutUsApresentation";
+import { ParagraphBlock } from "../../components/ParagraphBlock/ParagraphBlock";
 
 
 export function AboutUs() {
     return (
         <>
             <AboutUsApresentation />
+            <ParagraphBlock />
         </>
     )
 }
---
+++ src/components/ParagraphBlock/ParagraphBlock.tsx
@@ -0,0 +1,12 @@
+import styles from './ParagraphBlock.module.css';
+
+export function ParagraphBlock() {
+    return (
+        <>
+            <div className={styles.paragraphBlockContainer}>
+                <h3>Lorem Ipsum is simply dummy text of the printing. </h3>
+                <span>KODEX TECHNOLOGY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. We build and develop mobile applications for several top platforms, including Android  & IOS. </span>
+            </div>
+        </>
+    )
+}
