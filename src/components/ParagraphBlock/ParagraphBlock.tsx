27123f762a3c6603e6aa68cbd317598270475891
Felipe Borges <felipedefreitasborges@gmail.com>
2023-05-25 17:54:03 -0300

    Config ParagraphBloc and BlockContactUs

--- src/components/ParagraphBlock/ParagraphBlock.tsx
+++ src/components/ParagraphBlock/ParagraphBlock.tsx
@@ -5,7 +5,7 @@ export function ParagraphBlock() {
         <>
             <div className={styles.paragraphBlockContainer}>
                 <h3>Lorem Ipsum is simply dummy text of the printing. </h3>
-                <span>KODEX TECHNOLOGY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. We build and develop mobile applications for several top platforms, including Android  & IOS. </span>
+                <span>KODEX TECHNOLOGY (PVT) LTD is a team of experienced mobile and web applications and website builders measuring dozens of completed projects. We build and develop mobile applications for several top platforms, including Android  & IOS. We build and develop mobile applications for several top platforms, including Android  & IOS. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem corporis exercitationem dolorem maxime aspernatur minus, nemo aut esse animi unde praesentium numquam labore commodi architecto. Ipsam, ullam iste! Iusto, corporis! </span>
             </div>
         </>
     )
